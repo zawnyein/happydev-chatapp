451fbfdd4a80366e236549d677975306fedf0778
zawnyein <zawpluszaw@gmail.com>
2023-12-10 19:00:37 +0800

    testing uat with different ecs cluster code amendment

--- environments/uat/backend.tf
+++ environments/uat/backend.tf
@@ -3,6 +3,6 @@ terraform {
   backend "s3" {
     bucket  = "sctp-ce3-tfstate-bucket-1"  #Specify the name of S3 bucket where Terraform will store its state file
     key     = "environments/dev/deploy.tfstate" #Defines the key (path) within the S3 bucket where Terraform will store the state file for this specific environment
-    region  = "us-west-1" #Specifies the AWS region where the S3 bucket is located.
+    region  = "us-east-1" #Specifies the AWS region where the S3 bucket is located.
   }
 }
