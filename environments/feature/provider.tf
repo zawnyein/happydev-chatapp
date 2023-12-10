cde2e16d4ad1ee98c39501f584608e273a70ec70
zawnyein <zawpluszaw@gmail.com>
2023-12-10 12:33:44 +0800

    amended environment region for feature branch 2nd try

--- environments/feature/provider.tf
+++ environments/feature/provider.tf
@@ -1,4 +1,4 @@
-# This code is a Terraform configuration snippet that specifies the AWS provider and sets the AWS region to "us-west-1"
+# This code is a Terraform configuration snippet that specifies the AWS provider and sets the AWS region to "us-east-1"
 provider "aws" {
-  region = "us-west-1"
+  region = "us-east-1"
 }
